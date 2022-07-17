9664bc96ea4f1f784ca051ce7eba235bdab83f5e
lichengxian <59009875+lichengxian@users.noreply.github.com>
2022-07-17 20:59:09 +0800

    微信网页授权Demo

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const list = document.getElementById("list");
 const openId = document.getElementById("openId");
 const UserName = document.getElementById("UserName");
 const UserPhoto = document.getElementById("UserPhoto");
+// 公众号信息
+const appId = "wxbcc1eb17b5d50398";
+// 回调页
+const cbURL = encodeURIComponent(`http://127.0.0.1:5500/`);
+// 分享链接，静默授权
+const shareURL = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${cbURL}&response_type=code&scope=snsapi_base#wechat_redirect`;
+// 主动授权链接，额外带state参数
+const authURL = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${cbURL}&response_type=code&scope=snsapi_userinfo&state=auth#wechat_redirect`;
+// 判断微信平台
+const ua = navigator.userAgent.toLowerCase();
+const isWechat = /micromessenger/.test(ua);
 // AJAX请求
 const ajax = {
   get(url) {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
       let xhr = new XMLHttpRequest();
       xhr.open("GET", url);
       xhr.send();
@@ -14,27 +25,68 @@ const ajax = {
         if (xhr.readyState === 4) {
           if (xhr.status >= 200 && xhr.status < 300) {
             const res = JSON.parse(xhr.response);
-            resolve(res);
+            if (!res.code) resolve(res.data);
+            else reject(res);
           }
         }
       };
     });
   },
 };
+// 解析URL的query参数
+function query() {
+  const map = {};
+  const params = window.location.search.substring(1).split("&");
+  params.forEach((item) => {
+    const temp = item.split("=");
+    map[temp[0]] = temp[1];
+  });
+  return map;
+}
 // 初始化用户信息
 let UserInfo = {
-  openId: '',
-  UserName: '',
-  UserPhoto: '',
+  openId: "",
+  UserName: "",
+  UserPhoto: "",
 };
-
-const auth = () => {
-    list.style.display = "block";
-    openId.innerHTML = UserInfo.openId;
-    UserName.innerHTML = UserInfo.UserName;
-    UserPhoto.style.backgroundImage = `url(${UserInfo.UserPhoto})`;
+// 声明完以上内容，开始获取URL上的参数
+// 判断用户是否曾经主动授权过
+const auth = localStorage.getItem("Auth") || query().state || "";
+const code = query().code || "";
+const lastCode = sessionStorage.getItem("vercode");
+// 在微信平台中
+if (isWechat) {
+  // 储存本次code
+  sessionStorage.setItem("vercode", code);
+  // 二次分享无code，保证每次页面刷新，均存在有效的code，
+  if (code === lastCode || !code) {
+    window.location.replace(shareURL);
+  } else {
+    // 1是静默授权，0是主动授权
+    ajax
+      .get(`http://127.0.0.1:3000/auth?code=${code}&type=${auth ? 0 : 1}`)
+      .then((res) => {
+        console.log(res);
+        UserInfo.openId = res.openId;
+        if (res.UserName && res.UserPhoto) {
+          UserInfo.UserName = res.UserName;
+          UserInfo.UserPhoto = res.UserPhoto;
+          // 主动授权后，储存记录
+          localStorage.setItem("Auth", 1);
+        }
+      })
+      .then(() => {
+        openId.innerHTML = UserInfo.openId;
+        UserName.innerHTML = UserInfo.UserName;
+        UserPhoto.style.backgroundImage = `url(${UserInfo.UserPhoto})`;
+      })
+      .catch((e) => console.log(e.message));
+  }
+}
+// 按钮点击事件，获取主动授权
+const handleAuth = () => {
+  if (!auth) {
+    window.location.replace(authURL);
+  }
 };
-authBtn.addEventListener("click", auth);
-
-// 页面一挂载就发起请求
-ajax.get("http://127.0.0.1:3000/auth").then(res => UserInfo = res);
+authBtn.addEventListener("click", handleAuth);
--- server.js
+++ server.js
@@ -1,30 +1,82 @@
-const express = require('express');
-const https = require('https');
+const express = require("express");
+const https = require("https");
 // 公众号信息
-const appId = 'wxbcc1eb17b5d50398';
-const appSecret = '9ab1a2627e9ac81a2359708d8d7f6c9b';
-// 回调页
-const URL = 'http://127.0.0.1:5500/5.WeiXin/';
-// 开启一个服务器
+const appId = "wxbcc1eb17b5d50398";
+const appSecret = "9ab1a2627e9ac81a2359708d8d7f6c9b";
+// 创建一个服务器应用
 const app = express();
-// 跨域设置
-app.all('*', function(req, res, next) {
+// CORS跨域设置
+app.all("*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By",' 3.2.1')
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", " 3.2.1");
   res.header("Content-Type", "application/json;charset=utf-8");
   next();
 });
 // 接口
-app.get('/auth', function(req, res) {
-  res.send({
-    openId: "123",
-    UserName: "456",
-    UserPhoto: "",
-  });
-})
-
-app.listen(3000, function() {
-  console.log('服务器已开启');
+app.get("/auth", function (req, res) {
+  // 通过code获取AccessToken
+  const getAccessToken = (code) => {
+    return new Promise((resolve, reject) => {
+      const URL = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${appId}&secret=${appSecret}&code=${code}&grant_type=authorization_code`;
+      https
+        .get(URL, (res) => {
+          let rawData = "";
+          res.on("data", (data) => (rawData += data));
+          res.on("end", () => resolve(JSON.parse(rawData)));
+        })
+        .on("error", (e) => console.log(e.message));
+    });
+  };
+  // 通过AccessToken和openId获取UserInfo
+  const getUserInfo = (accessToken, openId) => {
+    return new Promise((resolve, reject) => {
+      const URL = `https://api.weixin.qq.com/sns/userinfo?access_token=${accessToken}&openid=${openId}&lang=zh_CN`;
+      https
+        .get(URL, (res) => {
+          let rawData = "";
+          res.on("data", (data) => (rawData += data));
+          res.on("end", () => resolve(JSON.parse(rawData)));
+        })
+        .on("error", (e) => console.log(e.message));
+    });
+  };
+  // 整体流程
+  const wxAuth = async () => {
+    // 解析query参数，字符串类型
+    const { code, type } = req.query;
+    const token = await getAccessToken(code);
+    // 请求报错
+    if (!token.access_token) {
+      return res.send({ code: token.errcode, message: token.errmsg });
+    }
+    // 静默授权
+    if (Number(type)) {
+      return res.send({
+        code: 0,
+        message: "success",
+        data: { openId: token.openid },
+      });
+      // 主动授权
+    } else {
+      const UserInfo = await getUserInfo(token.access_token, token.openid);
+      // 请求报错
+      if (!UserInfo.openid) {
+        return res.send({ code: UserInfo.errcode, message: UserInfo.errmsg });
+      }
+      return res.send({
+        code: 0,
+        message: "success",
+        data: {
+          openId: UserInfo.openid,
+          UserName: UserInfo.nickname,
+          UserPhoto: UserInfo.headimgurl,
+        },
+      });
+    }
+  };
+  wxAuth();
 });
+// 开启服务器
+app.listen(3000, () => console.log("服务器已开启"));
